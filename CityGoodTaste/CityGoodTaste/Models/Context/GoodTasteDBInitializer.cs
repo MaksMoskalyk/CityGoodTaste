736059cee0b2286fd3612a3895b35a2d348be339
Максим Москалик <maxim.moskalyk@gmail.com>
2017-03-09 19:54:33 +0200

    Transfer 4

--- CityGoodTaste/CityGoodTaste/Models/Context/GoodTasteDBInitializer.cs
+++ CityGoodTaste/CityGoodTaste/Models/Context/GoodTasteDBInitializer.cs
@@ -8,36 +8,36 @@ namespace CityGoodTaste
     {
         protected override void Seed(GoodTasteContext context)
         {
-            var userManager = new ApplicationUserManager(new UserStore<User>(context));
+            //var userManager = new ApplicationUserManager(new UserStore<User>(context));
 
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-            // создаем две роли
-            var role1 = new IdentityRole { Name = "admin" };
-            var role2 = new IdentityRole { Name = "moderator" };
-            var role3 = new IdentityRole { Name = "user" };
-            var role4 = new IdentityRole { Name = "guest" };
+            //// создаем две роли
+            //var role1 = new IdentityRole { Name = "admin" };
+            //var role2 = new IdentityRole { Name = "moderator" };
+            //var role3 = new IdentityRole { Name = "user" };
+            //var role4 = new IdentityRole { Name = "guest" };
 
-            // добавляем роли в бд
-            roleManager.Create(role1);
-            roleManager.Create(role2);
-            roleManager.Create(role3);
-            roleManager.Create(role4);
+            //// добавляем роли в бд
+            //roleManager.Create(role1);
+            //roleManager.Create(role2);
+            //roleManager.Create(role3);
+            //roleManager.Create(role4);
 
-            // создаем пользователей
-            var admin = new User { Login = "admin", Email = "somemail@mail.ru", UserName = "admin" };
-            string password = "Password!2";
-            var result = userManager.Create(admin, password);
+            //// создаем пользователей
+            //var admin = new User { Login = "admin", Email = "somemail@mail.ru", UserName = "admin" };
+            //string password = "Password!2";
+            //var result = userManager.Create(admin, password);
 
-            // если создание пользователя прошло успешно
-            if (result.Succeeded)
-            {
-                // добавляем для пользователя роль
-                userManager.AddToRole(admin.Id, role1.Name);
-                userManager.AddToRole(admin.Id, role2.Name);
-            }
+            //// если создание пользователя прошло успешно
+            //if (result.Succeeded)
+            //{
+            //    // добавляем для пользователя роль
+            //    userManager.AddToRole(admin.Id, role1.Name);
+            //    userManager.AddToRole(admin.Id, role2.Name);
+            //}
 
-            base.Seed(context);
+            //base.Seed(context);
         }
     }
 }
