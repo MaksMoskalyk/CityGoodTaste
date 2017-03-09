3a313d3f3fcede9c158a47f53c25a634430d1863
Timur Zhuravlev <tmrzhr777@outlook.com>
2017-03-09 11:33:25 -0800

    Auth Account Login works ok

--- CityGoodTaste/CityGoodTaste/Controllers/AccountController.cs
+++ CityGoodTaste/CityGoodTaste/Controllers/AccountController.cs
@@ -76,6 +76,7 @@ namespace CityGoodTaste.Controllers
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            bool aut = Request.IsAuthenticated;
             switch (result)
             {
                 case SignInStatus.Success:
--- CityGoodTaste/CityGoodTaste/Models/AccountViewModels.cs
+++ CityGoodTaste/CityGoodTaste/Models/AccountViewModels.cs
@@ -50,7 +50,7 @@ namespace CityGoodTaste.Models
     {
         [Required]
         [Display(Name = "Email")]
-        [EmailAddress]
+        //[EmailAddress]
         public string Email { get; set; }
 
         [Required]
