815c0223b418d1e4eaffe943aa7c026601dddcea
Максим Москалик <maxim.moskalyk@gmail.com>
2017-03-15 00:18:46 +0200

    ...

--- CityGoodTaste/CityGoodTaste/Models/Context/GoodTasteDBInitializer.cs
+++ CityGoodTaste/CityGoodTaste/Models/Context/GoodTasteDBInitializer.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace CityGoodTaste
 {
-    public class GoodTasteDBInitializer : CreateDatabaseIfNotExists<GoodTasteContext>
+    public class GoodTasteDBInitializer : DropCreateDatabaseAlways<GoodTasteContext>
     {
         protected override void Seed(GoodTasteContext context)
         {
