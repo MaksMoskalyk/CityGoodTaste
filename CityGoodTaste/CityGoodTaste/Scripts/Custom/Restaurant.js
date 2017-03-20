83dd73a710049ba180774754e35cc6315096d09e
Максим Москалик <maxim.moskalyk@gmail.com>
2017-03-20 18:49:55 +0200

    map

--- CityGoodTaste/CityGoodTaste/Scripts/Custom/Restaurant.js
+++ CityGoodTaste/CityGoodTaste/Scripts/Custom/Restaurant.js
@@ -27,13 +27,13 @@
 
     SetAjaxForShemaModals();
 
-    function myMap() {      
-        var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-        var mapProp = { center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId: google.maps.MapTypeId.ROADMAP };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-        var marker = new google.maps.Marker({ position: myCenter });
-        marker.setMap(map);
-    }
+    //function myMap() {      
+    //    var myCenter = new google.maps.LatLng(41.878114, -87.629798);
+    //    var mapProp = { center: myCenter, zoom: 12, scrollwheel: false, draggable: false, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    //    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    //    var marker = new google.maps.Marker({ position: myCenter });
+    //    marker.setMap(map);
+    //}
 
     $('#sumbitReservBt').click(function (event) {
         $('#closeModalReserv').click();
