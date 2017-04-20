51186dcdce2ff5b730d4f702fc8c69e266bf0ee9
Stepan1990 <stepan_sidorchuk@ukr.net>
2017-04-21 01:32:45 +0300

    Datepickers localization fix

--- CityGoodTaste/CityGoodTaste/Scripts/Custom/Account/ManageAccount.js
+++ CityGoodTaste/CityGoodTaste/Scripts/Custom/Account/ManageAccount.js
@@ -2,7 +2,7 @@
     $("div.form-group").addClass("formGroupCentered");
     $("div.input-group").addClass("inputGroupCentered");
     
-    $("label").addClass("control-label col-sm-3");
+    $("label").addClass("control-label col-sm-4");
 
     $("#manageAccountWrapper input").css("width", "240px");
     $("#manageAccountWrapper select").css("width", "240px");
